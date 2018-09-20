8fc051e31a394b992b3c6ff441488b397218cc26
Drew Cason <drcason@gmail.com>
2018-09-20 11:32:12 -0500

    isValidNumberFixed no class added

--- Exercise11/JS-Assignment/scripts/main.js
+++ Exercise11/JS-Assignment/scripts/main.js
@@ -4,7 +4,7 @@ var arrayItems = [];
 function update() {
     //////////////////////Your Code Starts here
 
-    
+
 
     ///Part 1
     var firstName = getValue('firstName');
@@ -15,19 +15,19 @@ function update() {
     document.getElementById('p1_fullName').innerHTML = `${firstName} ${lastName}`;
 
     checkNameValid = (firstName, lastName) => {
-        if(firstName && lastName) {
+        if (firstName && lastName) {
             return true;
         } else {
             return false;
         }
     };
 
-    
+
     document.getElementById('p1_valid').innerHTML = checkNameValid(firstName, lastName);
 
     var nameLength = firstName.length + lastName.length;
     document.getElementById('p1_fullNameLength').innerHTML = nameLength;
-    
+
     // Part 2
     var emailAddress = getValue('emailAddress');
     console.log(emailAddress);
@@ -35,7 +35,7 @@ function update() {
 
     var isValidEmail = (emailAddress.indexOf('@') > -1);
     document.getElementById('p2_valid').innerHTML = isValidEmail;
-        
+
     document.getElementById('p2_emailLength').innerHTML = emailAddress.length;
 
     // Part 3
@@ -43,20 +43,28 @@ function update() {
     console.log(randomNumber);
 
     document.getElementById('p3_number').innerHTML = randomNumber;
+
+    isValidNumber = (randomNumber) => {
+        let num = parseInt(randomNumber);
+        if (isNaN(num)) {
+            return false;
+        } else {
+            return true;
+        }
+    };
     
-    var isValidNumber = (typeof randomNumber === 'number');
-    document.getElementById('p3_valid').innerHTML = isValidNumber;
+    document.getElementById('p3_valid').innerHTML = isValidNumber(randomNumber);
 
     // Part 4
     var arrayInput = getValue('arrayValue');
     console.log(arrayInput);
 
     arrayItems.push(arrayInput);
-    document.getElementById('p4_arraylength').innerHTML = arrayItems.length; 
+    document.getElementById('p4_arraylength').innerHTML = arrayItems.length;
     document.getElementById('p4_valid').innerHTML = arrayItems;
     console.log(arrayItems);
 
-   addClassInvalidToName(firstName, lastName) {
+    addClassInvalidToName = (firstName, lastName) => {
         let elm = document.getElementById;
         if (!firstName && !lastName) {
             elm('firstName').classList.add("invalid");
@@ -77,7 +85,7 @@ function update() {
 }
 
 
-////
+
 var btn = document.getElementById('btnUpdate');
 btn.addEventListener('click', update);
 
@@ -88,4 +96,3 @@ function getValue(id) {
 function getElm(id) {
     return document.getElementById(id);
 }
-
