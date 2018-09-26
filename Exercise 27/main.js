775e2f8cebe4ea8f690ba2408947cc443f4202d7
Drew Cason <drcason@gmail.com>
2018-09-25 19:33:53 -0500

    exercise 12 complete

---
+++ Exercise 27/main.js
@@ -0,0 +1,27 @@
+/*//*
+    Exercise 27 
+*/
+$(document).ready(function(){
+    var email = email Input;
+    var patt = new RegExp(/@/i);
+    var res = patt.test(email);
+
+
+
+$('#btnvalidate').click(function(){
+    
+});
+
+
+
+    const numArray = [1,2,3,4,5];
+    
+    const sqNums = numArray.map((num) => {
+        return num*num;
+    });
+    console.log(sqNums);
+    
+
+
+
+});
